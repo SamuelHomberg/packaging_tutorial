6200f810b7a754893f43f45805c3af5c13f75379
s-homberg <85344885+s-homberg@users.noreply.github.com>
2024-03-07 11:29:16 -0600

    Add example class

--- src/example_package_samuelhomberg/example.py
+++ src/example_package_samuelhomberg/example.py
@@ -1,4 +1,4 @@
-def add_one(number):
+def add_one(number: int):
     """A function to add one to a number.
 
     Args:
@@ -8,3 +8,17 @@ def add_one(number):
         int: number + 1
     """
     return number + 1
+
+class ExampleClass():
+    """An example class doing example things.
+    """
+    def __init__(self):
+        self.var = 1
+
+    def get_var(self) -> int:
+        """Gets a variable of the class.
+
+        Returns:
+            int: Variable.
+        """
+        return self.var
